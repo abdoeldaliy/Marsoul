28590c0d1fd19c86eda1968113f89440f3109253
dwight202 <abdo50ahmed50@gmail.com>
2023-09-25 17:08:52 +0300

    Initial Commit

---
+++ Marsoul/MarsoulApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarsoulApp.swift
+//  Marsoul
+//
+//  Created by Mac on 22/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct MarsoulApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
